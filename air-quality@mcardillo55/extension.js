2dc91d757b5e7470463d2b8e7c3923b902011b9d
Mike Cardillo <mike@mjcardillo.com>
2020-08-28 23:52:52 -0700

    Added sample code and AQI label

--- air-quality@mcardillo55/extension.js
+++ air-quality@mcardillo55/extension.js
@@ -17,15 +17,63 @@
  */
 
 /* exported init */
+const Clutter = imports.gi.Clutter;
+const Gio = imports.gi.Gio;
+const GObject = imports.gi.GObject;
+const St = imports.gi.St;
+
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+
+const Config = imports.misc.config;
+const SHELL_MINOR = parseInt(Config.PACKAGE_VERSION.split('.')[1]);
+
+var AQIIndicator = class AQIIndicator extends PanelMenu.Button {
+    _init() {
+        super._init(0.0, `${Me.metadata.name} Indicator`, false)
+
+        this._aqi = new St.Label({
+            y_align: Clutter.ActorAlign.CENTER,
+            text: "250"
+        });
+        //this.actor.add_child(icon);
+
+        this.actor.add_child(this._aqi);
+
+        this.menu.addAction('Preferences', this.menuAction, null);
+    }
+
+    menuAction() {
+        log('Menu item activated');
+    }
+}
+
+if (SHELL_MINOR > 30) {
+    AQIIndicator = GObject.registerClass(
+        {GTypeName: 'AQIIndicator'},
+        AQIIndicator
+    )
+}
 
 class Extension {
     constructor() {
+        this.indicator = null;
     }
 
     enable() {
+        log(`enabling ${Me.metadata.name} version ${Me.metadata.version}`);
+        this.indicator = new AQIIndicator();
+        Main.panel.addToStatusArea(`${Me.metadata.name} Indicator`, this.indicator);
     }
 
     disable() {
+        log(`disabling ${Me.metadata.name} version ${Me.metadata.version}`)
+        if (this.indicator !== null) {
+            this.indicator.destroy();
+            this.indicator = null;
+        }
     }
 }
 
