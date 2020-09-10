bc6c37f466e342359ff0c390f43a6630bc0b8cd0
Mike Cardillo <mike@mjcardillo.com>
2020-09-09 17:41:19 -0700

    Added address search via OpenStreetMap

--- air-quality@mcardillo55/prefs.js
+++ air-quality@mcardillo55/prefs.js
@@ -1,9 +1,11 @@
 'use strict';
 
+const Soup = imports.gi.Soup;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
 const GLib = imports.gi.GLib;
 const Gtk = imports.gi.Gtk;
+const Gdk = imports.gi.Gdk;
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
@@ -11,6 +13,9 @@ const Lang = imports.lang;
 
 const EXTENSIONDIR = Me.dir.get_path();
 const AIRQUALITY_SETTINGS_SCHEMA = 'org.gnome.shell.extensions.airquality';
+const OPENWEATHER_URL_OSM = 'https://nominatim.openstreetmap.org/search';
+
+let _httpSession;
 
 const AirQualityPrefsWidget = new GObject.Class({
     Name: 'AirQualityExtension.Prefs.Widget',
@@ -28,10 +33,14 @@ const AirQualityPrefsWidget = new GObject.Class({
     Window: new Gtk.Builder(),
 
     initWindow: function() {
+        this.user_agent = Me.metadata.uuid;
+
         this.Window.add_from_file(EXTENSIONDIR + "/air-quality-settings.ui");
 
         this.mainWidget = this.Window.get_object("prefs-widget")
         this.currentSensor = this.Window.get_object("current-sensor")
+        this.searchLocation = this.Window.get_object("search-location")
+        this.searchMenu = this.Window.get_object("search-menu")
 
         this.currentSensor.connect("activate", Lang.bind(this, function() {
             this.Settings.set_string("current-sensor", this.currentSensor.get_text())
@@ -40,10 +49,102 @@ const AirQualityPrefsWidget = new GObject.Class({
         this.loadConfig();
 
         this.currentSensor.set_text(this.Settings.get_string("current-sensor"))
+
+        this.Window.get_object("search-button").connect("clicked", Lang.bind(this, function() {
+            this.clearSearchMenu();
+            
+            let params = {
+                format: 'json',
+                addressdetails: '1',
+                q: this.searchLocation.get_text()
+            };
+            this.load_json_async(OPENWEATHER_URL_OSM, params, Lang.bind(this, function() {
+                if (!arguments[0]) {
+                    let item = new Gtk.MenuItem({
+                        label: _("Invalid data when searching for \"%s\"").format(location)
+                    });
+                    this.searchMenu.append(item);
+                } else {
+                    let newCity = arguments[0];
+
+                    if (Number(newCity.length) < 1) {
+                        let item = new Gtk.MenuItem({
+                            label: _("\"%s\" not found").format(location)
+                        });
+                        this.searchMenu.append(item);
+                    } else {
+                        var m = {};
+                        for (var i in newCity) {
+
+                            let cityText = newCity[i].display_name;
+                            let cityCoord = "[" + newCity[i].lat + "," + newCity[i].lon + "]";
+
+                            let item = new Gtk.MenuItem({
+                                label: cityText + " " + cityCoord
+                            });
+                            //item.connect("activate", Lang.bind(this, this.onActivateItem));
+                            this.searchMenu.append(item);
+                        }
+                    }
+                }
+                this.showSearchMenu();
+            }));
+        }));
     },
 
     loadConfig: function() {
         this.Settings = ExtensionUtils.getSettings(AIRQUALITY_SETTINGS_SCHEMA);
+    },
+
+    load_json_async(url, params, fun) {
+        if (_httpSession === undefined) {
+            _httpSession = new Soup.Session();
+            _httpSession.user_agent = this.user_agent;
+        } else {
+            // abort previous requests.
+            _httpSession.abort();
+        }
+
+        let message = Soup.form_request_new_from_hash('GET', url, params);
+
+        _httpSession.queue_message(message, Lang.bind(this, function(_httpSession, message) {
+            try {
+                if (!message.response_body.data) {
+                    fun.call(this, 0);
+                    return;
+                }
+                let jp = JSON.parse(message.response_body.data);
+                fun.call(this, jp);
+            } catch (e) {
+                fun.call(this, 0);
+                return;
+            }
+        }));
+        return;
+    },
+
+    clearSearchMenu: function() {
+        let children = this.searchMenu.get_children();
+        for (let i in children) {
+            this.searchMenu.remove(children[i]);
+        }
+    },
+
+    showSearchMenu: function() {
+        this.searchMenu.show_all();
+        if (typeof this.searchMenu.popup_at_widget === "function") {
+            this.searchMenu.popup_at_widget(this.searchLocation, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, null);
+        }
+        else
+        {
+            this.searchMenu.popup(null, null, Lang.bind(this, this.placeSearchMenu), 0, this.searchLocation);
+        }
+    },
+
+    placeSearchMenu: function() {
+        let[gx, gy, gw, gh] = this.searchName.get_window().get_geometry();
+        let[px, py] = this.searchName.get_window().get_position();
+        return [gx + px, gy + py + this.searchName.get_allocated_height()];
     }
 });
 
